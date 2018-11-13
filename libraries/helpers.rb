bf31ad8ad5fde34c2b81e7573b09834218c46d73
Paul Holden <paul@holdensoftware.com>
2018-11-12 21:29:37 -0800

    Add up_to_date? helper

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -4,7 +4,16 @@ class Chef
       require 'git'
 
       def clone_repo
-        ::Git::Base.clone(new_resource.repository, 'repo', bare: true, path: new_resource.destination)
+        ::Git::Base.clone(new_resource.repository, 'repo.git', bare: true, path: new_resource.destination)
+      end
+
+      # Check if the HEAD revision matches the remote branch
+      def up_to_date?
+        repo = ::Git::Base.bare("#{new_resource.destination}/repo.git")
+        head = repo.object('HEAD')
+        remote = ::Git.ls_remote("#{new_resource.destination}/repo.git")
+        branch = remote['branches'][new_resource.branch]
+        branch[:sha] == head.sha
       end
     end
   end
