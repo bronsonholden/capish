9afa0ed33ebf16c8e9b9a606711cf0e8cb4d266e
Paul Holden <pholden@stria.com>
2018-11-15 10:33:59 -0800

    Use Git namespace methods for brevity

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -4,12 +4,12 @@ class Chef
       require 'git'
 
       def clone_repo
-        ::Git::Base.clone(new_resource.repository, 'repo.git', bare: true, path: new_resource.destination)
+        ::Git.clone(new_resource.repository, 'repo.git', bare: true, path: new_resource.destination)
       end
 
       # Check if the HEAD revision matches the remote branch
       def up_to_date?
-        repo = ::Git::Base.bare("#{new_resource.destination}/repo.git")
+        repo = ::Git.bare("#{new_resource.destination}/repo.git")
         head = repo.object('HEAD')
         remote = ::Git.ls_remote("#{new_resource.destination}/repo.git")
         branch = remote['branches'][new_resource.branch]
