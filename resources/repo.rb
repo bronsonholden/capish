0f198d2501efe670334b09e5115a641d8078c572
Paul Holden <pholden@stria.com>
2018-11-16 10:22:00 -0800

    Checkouts are now contained in "releases" folder

--- resources/repo.rb
+++ resources/repo.rb
@@ -15,7 +15,7 @@ action :checkout do
   # Resource name
   name = "checkout repo #{new_resource.repository}"
   # The canonical path of the new checkout
-  checkout_path = "#{new_resource.destination}/#{ts}"
+  checkout_path = "#{new_resource.destination}/releases/#{ts}"
 
   directory checkout_path do
     not_if { up_to_date? }
@@ -35,12 +35,8 @@ action :checkout do
   ruby_block name do
     action :nothing
     block do
-      repo = ::Git.clone(new_resource.repository, ts, path: new_resource.destination)
-      if !new_resource.branch.nil?
-        repo.checkout(new_resource.branch)
-      elsif !new_resource.tag.nil?
-        repo.checkout(new_resource.tag)
-      end
+      repo = ::Git.clone(new_resource.repository, ts, path: "#{new_resource.destination}/releases")
+      repo.checkout(new_resource.branch || new_resource.tag)
     end
   end
 end
--- test/cookbooks/capish_repo_test/recipes/default.rb
+++ test/cookbooks/capish_repo_test/recipes/default.rb
@@ -1,5 +1,5 @@
 capish_repo 'capish test repo' do
   repository 'https://github.com/paulholden2/capish-test'
   destination '/var/www/capish-test'
-  tag 'deploy'
+  branch 'deploy'
 end
