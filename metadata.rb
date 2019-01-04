dbf2727522356e3190414e1c5a0a47002552e6c2
Paul Holden <pholden@stria.com>
2019-01-04 08:03:17 -0800

    v0.2.1
    [ci skip]
    
    - Fix issue with Travis build

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'paul@holdensoftware.com'
 license 'ISC'
 description 'Simple Capistrano-like code deployment.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.2.1'
 chef_version '>= 13.0'
 supports 'ubuntu'
 issues_url 'https://github.com/paulholden2/capish/issues'
