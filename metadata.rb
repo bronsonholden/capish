26b84f12da7eff68bf01e37930c1067bcfe32788
Paul Holden <pholden@stria.com>
2019-01-30 10:39:24 -0800

    v0.3.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'paul@holdensoftware.com'
 license 'ISC'
 description 'Simple Capistrano-like code deployment.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.1'
+version '0.3.2'
 chef_version '>= 13.0'
 supports 'ubuntu'
 issues_url 'https://github.com/paulholden2/capish/issues'
